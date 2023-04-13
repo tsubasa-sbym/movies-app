c202b888edb15cf86fbc1bba10daadcfd7d59f9c
tsubasa-sbym <gauvm59mx@yahoo.co.jp>
2023-04-13 23:44:06 +0900

    feat: App.tsxにRowコンポーネントを設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Row } from './components/Row';
+import { requests } from "./request";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Row
+        title="NETFLIX ORIGUINALS"
+        fetchUrl={requests.feachNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Top Rated" fetchUrl={requests.feactTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.feactActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.feactComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.feactHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.feactRomanceMovies} />
+      <Row title="DOcumentaries" fetchUrl={requests.feactDocumentMovies} />
     </div>
   );
 }
